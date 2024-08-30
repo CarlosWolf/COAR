49e67b203f61ba52cce4dd72adde158a1f8e6687
_carlosmtz <carlos@consultoresweb.com.mx>
2024-08-29 23:35:22 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let imagenes = ['img/chetumal.jpg','img/bacalar.jpg','img/tulum.jpg']
+cont = 0;
+function carrousel (contenedor){
+	contenedor.addEventListener('click', e =>{
+		let atras = contenedor.querySelector('.atras'),
+	});
+}

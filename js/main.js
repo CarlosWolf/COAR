1e07ec32d4b0c9e622fda250391eaa3ef2febd0d
CarlosWolf <itrwolf117@gmail.com>
2024-10-31 01:44:10 -0500

    Ajustes hero

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ let imagenes = ['img/chetumal.jpg','img/bacalar.jpg','img/tulum.jpg']
 cont = 0;
 function carrousel (contenedor){
 	contenedor.addEventListener('click', e =>{
-		let atras = contenedor.querySelector('.atras'),
+		let atras = contenedor.querySelector('.atras')
 	});
 }

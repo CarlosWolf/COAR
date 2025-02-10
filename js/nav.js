437f24b51006b6e2130e22fd804b07bc449a404a
_carlosmtz <carlos@consultoresweb.com.mx>
2025-02-10 16:44:27 -0500

    efectos de logo con menu

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const toggleButton = document.querySelector('.menu-toggle'); // Selecciona el botón de menú
 	const menu = document.querySelector('.menu'); // Selecciona el contenedor con clase 'menu'
+    const image = document.querySelector('.textos'); // Selecciona la imagen
 
 	toggleButton.addEventListener('click', () => {
 		// Alternar la clase 'column' en el menú
@@ -13,5 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
 		// Actualizar el atributo aria-expanded para accesibilidad
 		const expanded = toggleButton.getAttribute('aria-expanded') === 'true';
 		toggleButton.setAttribute('aria-expanded', !expanded);
+
+        // Alternar la visibilidad de la imagen
+        if (image) {
+            image.style.display = image.style.display === 'none' ? 'block' : 'none';
+        }
+
 	});
 });
